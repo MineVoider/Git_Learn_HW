48d4a48ac8d13c9810b9ded9dce7523f004cd72f
VoidMiner <VoidMiner@163.com>
2024-05-11 10:39:23 +0800

    内训代码

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/main.obj"
+  "CMakeFiles/main.dir/src/main.obj.d"
+  "CMakeFiles/main.dir/src/print.obj"
+  "CMakeFiles/main.dir/src/print.obj.d"
+  "CMakeFiles/main.dir/src/print1.obj"
+  "CMakeFiles/main.dir/src/print1.obj.d"
+  "D:/Program/Test/bin/libmain.dll.a"
+  "D:/Program/Test/bin/main.exe"
+  "D:/Program/Test/bin/main.exe.manifest"
+  "D:/Program/Test/bin/main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
